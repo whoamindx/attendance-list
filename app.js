3d5685382a631684dc9cbb9e4efc284616c44fe8
Gabriel Napoleão <gabriel.ndr.sky@gmail.com>
2017-09-14 10:30:43 -0300

    Fix preventDefault and add Edit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-$('#form').submit(function(){
+$('#form').submit(function(e){
 	// Do not reload page
-	event.preventDefault();
+	e.preventDefault();
 	// Take form data and save in LocalStorage
 	const dataArray = $(this).serializeArray();
 	let dataObject = {};
@@ -18,18 +18,21 @@ $('#form').submit(function(){
 	repeat();
 })
 let repeat = function(){
+	let i = 0;
 	for (x in localStorage){
 		var y = JSON.parse(localStorage[x]);
-		$('#cards').append(`<div class="card blue-grey darken-1">
-										<div class="card-content white-text">
+		$('#cards').append(`<div class="card blue-grey darken-1" id="info-${i}">
+										<div class="card-content white-text" style="overflow: hidden;">
 											<span class="card-title">${y.name}</span>
 											<p>${y.subject}</p>
 										</div>
 										<div class="card-action">
 											<a href="#" onclick="event.preventDefault()">Ramal: ${y.ramal}</a>
-											<a class="waves-effect waves-light btn" onclick="deleta('${y.data}')">Excluir</a>
+											<a class="waves-effect waves-light btn" id="excluir" onclick="deleta('${y.data}')">Excluir</a>
+											<a class="waves-effect waves-light btn" id="editar" onclick="edita('${y.data}','info-${i}','${y.ramal}')">Editar</a>
 										</div>
 									</div>`)
+		i++;
 	}
 }
 // Delete the data based on the key (date)
@@ -39,8 +42,39 @@ let deleta = function(date){
 	$('#cards').empty();
 	repeat();
 }
-// Once you load the page, load the data
-window.onload = () => repeat();
+
+// Edit the data based on the key (date)
+let edita = function(date,i,ramal){
+	let dataTratada = new Date(date);
+
+	let oldTitle = $("#"+i+' .card-title').html();
+	$("#"+i+' .card-content .card-title').html(`<div class="input-field col s12">
+																<input id="new-name" name="new-name" type="text" class="validate" value="${oldTitle}" required pattern="[A-Za-z]{3,}">
+																<label for="new-name" data-error="Mínimo 3 caracteres" data-success="Ok"></label>
+															</div>`);
+
+	let oldSubject = $("#"+i+' .card-content p').html();
+	$("#"+i+' .card-content p').html(`<div class="input-field col s12">
+													<textarea id="new-subject" name="new-subject" class="materialize-textarea validate" required>${oldSubject}</textarea>
+													<label for="new-subject"></label>
+												</div>`);
+
+	$("#"+i+' #excluir').remove();
+	$("#"+i+' #editar').remove();
+	$("#"+i+' .card-action').append(`<a class="waves-effect waves-light btn" id="editar" onclick="update('${dataTratada}','${ramal}')">Concluir</a>`);
+	// repeat();
+}
+
+let update = function(data, ramal){
+	let name = $('#new-name').val();
+	let subject = $('#new-subject').val();
+	let obj = {name: name, ramal: ramal, subject: subject, data: new Date(data)}
+	obj = JSON.stringify(obj);
+	localStorage.setItem(data, obj);
+	$('#cards').empty();
+	repeat();
+}
+
 
 // Backup
 document.getElementById('backup').addEventListener("click",function(){
@@ -57,3 +91,7 @@ document.getElementById('import').addEventListener("click",function(){
 	repeat();
 	document.getElementById('import-code').value = "";
 });
+
+
+// Once you load the page, load the data
+window.onload = () => repeat();
